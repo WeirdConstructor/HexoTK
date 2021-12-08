a507537a15072c871aea21863b11e04bd2119ea7
Weird Constructor <weirdconstructor@gmail.com>
2021-12-08 05:05:35 +0100

    Rewrote how the WichText widget retrieves it's data

--- src/widgets/wichtext.rs
+++ src/widgets/wichtext.rs
@@ -15,60 +15,31 @@ use std::collections::HashMap;
 use std::str::Chars;
 use std::iter::Peekable;
 
-pub trait WichTextValueSource {
-    fn set(&self, key: &str, v: f32);
-    fn value(&self, key: &str) -> f32;
-    fn clamp(&self, key: &str, v: f32) -> f32;
-    fn map_knob(&self, key: &str, v: f32) -> f32;
-    fn step(&self, key: &str) -> f32;
-    fn fmt(&self, key: &str, v: f32, buf: &mut [u8]) -> usize;
-}
+pub trait WichTextData: crate::Mutable {
+    /// The text of WichText. If you change this, you need to increase
+    /// the [WichTextData::text_generation] to let WichText reparse it.
+    fn text(&self) -> String;
+    /// The generation counter tells WichText when to reparse the output of
+    /// [WichTextData::text].
+    fn text_generation(&self) -> usize;
 
-impl WichTextValueSource for RefCell<HashMap<String, (f32, f32, u8)>> {
-    fn set(&self, key: &str, new_v: f32) {
-        let mut create = false;
+    /// Change the knobs normalized value to `v`.
+    fn knob_set(&self, key: &str, v: f32);
+    /// Retrieve the knobs normalized value in the range 0.0 to 1.0.
+    fn knob_value(&self, key: &str) -> f32;
+    /// Clamp the knob value to a range inside 0.0 to 1.0
+    fn knob_clamp(&self, key: &str, v: f32) -> f32;
+    /// Map the normalized value to the UI range
+    fn knob_map_ui_range(&self, key: &str, v: f32) -> f32;
+    /// The step of a knob that is applied when dragging the knob.
+    fn knob_step(&self, key: &str) -> f32;
+    /// Format the value to text, here you can calculate the denormalized
+    /// value for the given normalized value `v` and write the label
+    /// text to `buf`.
+    fn knob_fmt(&self, key: &str, v: f32, buf: &mut [u8]) -> usize;
 
-        if let Some((v, _, _)) = self.borrow_mut().get_mut(key) {
-            *v = new_v;
-        } else {
-            create = true;
-        }
-
-        if create {
-            self.borrow_mut().insert(key.to_string(), (new_v, 0.05, 2));
-        }
-    }
-
-    fn map_knob(&self, _key: &str, v: f32) -> f32 {
-        v.clamp(0.0, 1.0)
-    }
-
-    fn clamp(&self, _key: &str, v: f32) -> f32 {
-        v.clamp(0.0, 1.0)
-    }
-
-    fn value(&self, key: &str) -> f32 {
-        self.borrow().get(key).map(|v| v.0).unwrap_or(0.0)
-    }
-
-    fn step(&self, key: &str) -> f32 {
-        self.borrow().get(key).map(|v| v.1).unwrap_or(0.05)
-    }
-
-    fn fmt(&self, key: &str, v: f32, buf: &mut [u8]) -> usize {
-        use std::io::Write;
-        let mut bw = std::io::BufWriter::new(buf);
-
-        let prec =
-            if let Some((_, _, prec)) = self.borrow().get(key) {
-                *prec
-            } else { 2 };
-
-        match write!(bw, "{:5.prec$}", v, prec = prec as usize) {
-            Ok(_) => bw.buffer().len(),
-            _     => 0,
-        }
-    }
+    /// Retrieve the graph data source for the given `key`.
+    fn data_source(&self, key: &str) -> Option<Rc<dyn WichTextDataSource>>;
 }
 
 pub trait WichTextDataSource {
@@ -87,10 +58,86 @@ impl WichTextDataSource for Rc<RefCell<Vec<f32>>> {
     fn sample(&self, i: usize) -> f32 { self.borrow().get(i).copied().unwrap_or(0.0) }
 }
 
-pub enum WichTextMessage {
-    SetText(String),
-    SetDataSource(String, Rc<dyn WichTextDataSource>),
-    SetValueSource(Rc<dyn WichTextValueSource>),
+#[derive(Debug)]
+struct WichTextSimpleDataStoreImpl {
+    text:           String,
+    text_gen:       usize,
+
+    knobs:          HashMap<String, f32>,
+    data_sources:   HashMap<String, Rc<Vec<f32>>>,
+
+    changed:        bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct WichTextSimpleDataStore(Rc<RefCell<WichTextSimpleDataStoreImpl>>);
+
+impl WichTextSimpleDataStore {
+    pub fn new() -> Self {
+        Self(Rc::new(RefCell::new(WichTextSimpleDataStoreImpl {
+            text:           String::from(""),
+            text_gen:       0,
+            knobs:          HashMap::new(),
+            data_sources:   HashMap::new(),
+            changed:        false,
+        })))
+    }
+}
+
+impl WichTextData for WichTextSimpleDataStore {
+    fn text(&self) -> String { self.0.borrow().text.clone() }
+    fn text_generation(&self) -> usize { self.0.borrow().text_gen }
+
+    fn knob_set(&self, key: &str, v: f32) {
+        let mut bor = self.0.borrow_mut();
+        if let Some(val) = bor.knobs.get_mut(key) {
+            *val = v;
+        } else {
+            bor.knobs.insert(key.to_string(), v);
+        }
+
+        bor.changed = true;
+    }
+
+    fn knob_value(&self, key: &str) -> f32 {
+        self.0.borrow().knobs.get(key).copied().unwrap_or(0.0)
+    }
+
+    fn knob_clamp(&self, key: &str, v: f32) -> f32 { v.clamp(0.0, 1.0) }
+    fn knob_map_ui_range(&self, key: &str, v: f32) -> f32 { v.clamp(0.0, 1.0) }
+    fn knob_step(&self, key: &str) -> f32 { 0.05 }
+    fn knob_fmt(&self, key: &str, v: f32, buf: &mut [u8]) -> usize {
+        use std::io::Write;
+        let mut bw = std::io::BufWriter::new(buf);
+
+//        let prec =
+//            if let Some((_, _, prec)) = self.borrow().get(key) {
+//                *prec
+//            } else { 2 };
+        let prec = 2;
+
+        match write!(bw, "{:5.prec$}", v, prec = prec as usize) {
+            Ok(_) => bw.buffer().len(),
+            _     => 0,
+        }
+    }
+
+    fn data_source(&self, key: &str) -> Option<Rc<dyn WichTextDataSource>> {
+        if let Some(data) = self.0.borrow().data_sources.get(key).cloned() {
+            Some(data)
+        } else {
+            None
+        }
+    }
+}
+
+impl crate::Mutable for WichTextSimpleDataStore {
+    fn check_change(&mut self) -> bool {
+        let mut ch_borrow = self.0.borrow_mut();
+        let is_changed = ch_borrow.changed;
+        ch_borrow.changed = false;
+        is_changed
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -191,13 +238,16 @@ impl WTFragment {
         }
     }
 
-    fn draw<F: for<'a> Fn(&str, &'a mut [u8]) -> (&'a str, f32)>(&self, p: &mut Painter,
+    fn draw<F: for<'a> Fn(&str, &'a mut [u8]) -> (&'a str, f32)>(
+        &self,
+        p: &mut Painter,
+        data:         &Rc<dyn WichTextData>,
         data_sources: &mut HashMap<String, DataSource>,
-        fetch_value: &F, // &Rc<dyn WichTextValueSource>,
-        pos:   Rect,
-        bg_clr:(f32, f32, f32),
-        color: (f32, f32, f32),
-        color2: (f32, f32, f32),
+        fetch_value: &F,
+        pos:        Rect,
+        bg_clr:     (f32, f32, f32),
+        color:      (f32, f32, f32),
+        color2:     (f32, f32, f32),
         orig_color: (f32, f32, f32))
     {
         match &self.typ {
@@ -215,6 +265,13 @@ impl WTFragment {
                 if let Some(src) = data_sources.get_mut(key) {
                     src.create_points(graph_w, graph_h);
                     src.draw(p, pos.x, pos.y, color2);
+                } else {
+                    if let Some(src) = data.data_source(key) {
+                        let mut data_src = DataSource::new(src);
+                        data_src.create_points(graph_w, graph_h);
+                        data_src.draw(p, pos.x, pos.y, color2);
+                        data_sources.insert(key.to_string(), data_src);
+                    }
                 }
 
                 p.label_mono(
@@ -453,30 +510,27 @@ impl DataSource {
 }
 
 pub struct WichText {
-    new_text:       Option<String>,
-    lines:          Vec<WTLine>,
-    wrapped_lines:  Vec<WTLine>,
-    full_h:         f32,
-    last_width:     i64,
+    data:               Rc<dyn WichTextData>,
+    text_generation:    usize,
 
-    zones:          Vec<(Rect, usize, usize)>,
+    lines:              Vec<WTLine>,
+    wrapped_lines:      Vec<WTLine>,
+    full_h:             f32,
+    last_width:         i64,
 
-    hover:          Option<(usize, usize)>,
-    active:         Option<(usize, usize)>,
-    drag:           Option<(f32, f32, f32, f32, f32, String)>,
+    zones:              Vec<(Rect, usize, usize)>,
 
-    scroll:         (f32, f32),
-    render:         (f32, f32),
-    pan_pos:        Option<(f32, f32)>,
+    hover:              Option<(usize, usize)>,
+    active:             Option<(usize, usize)>,
+    drag:               Option<(f32, f32, f32, f32, f32, String)>,
 
-    data_sources:   HashMap<String, DataSource>,
-    value_source:   Rc<dyn WichTextValueSource>,
+    scroll:             (f32, f32),
+    render:             (f32, f32),
+    pan_pos:            Option<(f32, f32)>,
 
-    mouse_pos:      (f32, f32),
+    data_sources:       HashMap<String, DataSource>,
 
-//    on_click:       Option<Box<dyn Fn(&mut Self, &mut State, Entity, usize, usize, &str)>>,
-//    on_value:       Option<Box<dyn Fn(&mut Self, &mut State, Entity, usize, usize, &str, f32)>>,
-//    on_hover:       Option<Box<dyn Fn(&mut Self, &mut State, Entity, bool, usize)>>,
+    mouse_pos:          (f32, f32),
 }
 
 fn parse_key(ci: &mut Peekable<Chars<'_>>) -> String {
@@ -508,30 +562,33 @@ fn parse_number<T: std::str::FromStr>(ci: &mut Peekable<Chars<'_>>, default: T) 
 }
 
 impl WichText {
-    pub fn new() -> Self {
+    pub fn new(data: Rc<dyn WichTextData>) -> Self {
         Self {
-            new_text:       None,
-            lines:          vec![],
-            wrapped_lines:  vec![],
-            full_h:         0.0,
-            last_width:     0,
-            data_sources:   HashMap::new(),
-            value_source:   Rc::new(RefCell::new(HashMap::new())),
+            data,
+            text_generation: 0,
 
-            zones:          vec![],
+            lines:           vec![],
+            wrapped_lines:   vec![],
+            full_h:          0.0,
+            last_width:      0,
+            data_sources:    HashMap::new(),
 
-            scroll:         (0.0, 0.0),
-            render:         (0.0, 0.0),
-            pan_pos:        None,
+            zones:           vec![],
 
-            hover:          None,
-            active:         None,
-            drag:           None,
+            scroll:          (0.0, 0.0),
+            render:          (0.0, 0.0),
+            pan_pos:         None,
 
-            mouse_pos:      (0.0, 0.0),
+            hover:           None,
+            active:          None,
+            drag:            None,
+
+            mouse_pos:       (0.0, 0.0),
         }
 
     }
+
+    pub fn data_mut(&mut self) -> &mut Rc<dyn WichTextData> { &mut self.data }
 
 //    pub fn on_value<F>(mut self, on_value: F) -> Self
 //    where
@@ -813,26 +870,6 @@ impl WichText {
     pub fn handle(
         &mut self, w: &Widget, event: &InputEvent, out_events: &mut Vec<(usize, Event)>)
     {
-//        if let Some(wt_msg) = event.message.downcast::<WichTextMessage>() {
-//            match wt_msg {
-//                WichTextMessage::SetText(text) => {
-//                    self.new_text = Some(text.clone());
-//
-//                    state.insert_event(
-//                        Event::new(WindowEvent::Redraw)
-//                        .target(Entity::root()));
-//                },
-//                WichTextMessage::SetDataSource(key, src) => {
-//                    self.data_sources.insert(
-//                        key.to_string(),
-//                        DataSource::new(src.clone()));
-//                },
-//                WichTextMessage::SetValueSource(src) => {
-//                    self.value_source = src.clone();
-//                },
-//            }
-//        }
-
         match event {
             InputEvent::MouseButtonPressed(btn) => {
                 let (x, y) = self.mouse_pos;
@@ -848,8 +885,8 @@ impl WichText {
                         if let Some(FragType::Value { key }) =
                             self.get(line, frag).map(|f| &f.typ)
                         {
-                            let s = self.value_source.step(&key);
-                            let v = self.value_source.value(&key);
+                            let s = self.data.knob_step(&key);
+                            let v = self.data.knob_value(&key);
 
                             self.drag = Some((x, y, s, v, v, key.to_string()));
                             w.activate();
@@ -875,8 +912,8 @@ impl WichText {
                             self.drag.take()
                         {
                             let new_val =
-                                self.value_source.clamp(&key, new_val);
-                            self.value_source.set(&key, new_val);
+                                self.data.knob_clamp(&key, new_val);
+                            self.data.knob_set(&key, new_val);
                         }
 
                     } else if self.active.is_some() && self.active == cur {
@@ -953,9 +990,11 @@ impl WichText {
             h: pos.h,
         };
 
-        if let Some(new_text) = self.new_text.take() {
-            self.parse(style.font_size, p, &new_text);
+        if self.text_generation != self.data.text_generation() {
+            self.parse(style.font_size, p, &self.data.text());
             self.last_width = 0;
+
+            self.text_generation = self.data.text_generation();
         }
 
         let width = pos.w.floor() as i64;
@@ -970,22 +1009,22 @@ impl WichText {
 
         let (_scroll_x, scroll_y) = self.clamp_scroll(0.0, 0.0);
 
-        let val_src = self.value_source.clone();
+        let val_src = self.data.clone();
         let drag = self.drag.clone();
 
         let fetch_value
             : Box<dyn for<'a> Fn(&str, &'a mut [u8]) -> (&'a str, f32)> =
             Box::new(move |key: &str, buf: &mut [u8]| {
-                let v = val_src.value(&key);
+                let v = val_src.knob_value(&key);
                 let v =
                     if let Some((_, _, _, _, tmp, drag_key)) = &drag {
                         if &key == &drag_key { *tmp }
                         else { v }
                     } else { v };
 
-                let v     = val_src.clamp(&key, v);
-                let knb_v = val_src.map_knob(&key, v);
-                let len   = val_src.fmt(&key, v, &mut buf[..]);
+                let v     = val_src.knob_clamp(&key, v);
+                let knb_v = val_src.knob_map_ui_range(&key, v);
+                let len   = val_src.knob_fmt(&key, v, &mut buf[..]);
                 let val_s = std::str::from_utf8(&buf[0..len]).unwrap();
 
                 (val_s, knb_v)
@@ -1034,6 +1073,7 @@ impl WichText {
 
                 frag.draw(
                     p,
+                    &self.data,
                     &mut self.data_sources,
                     &fetch_value,
                     frag_pos,
